d3545fde8343731ad6b24bcafe9ec438c4825f61
sivan67906 <rajat.sivan@gmail.com>
2024-09-30 21:46:31 +0530

    Add project files.

---
+++ EMS.Web/Controllers/AdministrationController.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Viewmodels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMS.Web.Controllers;
+[Authorize]
+public class AdministrationController : Controller
+{
+    private readonly RoleManager<IdentityRole> roleManager;
+
+    public AdministrationController(RoleManager<IdentityRole> roleManager)
+    {
+        this.roleManager = roleManager;
+    }
+
+    [HttpGet]
+    public IActionResult CreateRole()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            IdentityRole identityRole = new IdentityRole
+            {
+                Name = model.RoleName
+            };
+
+            IdentityResult result = await roleManager.CreateAsync(identityRole);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Employee");
+            }
+
+            foreach (IdentityError error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
+
+        return View(model);
+    }
+
+}
